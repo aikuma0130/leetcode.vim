439b60547814bdd23dc7c9040ee957f1603e2450
Dakie <ktaka0130@gmail.com>
2019-12-17 03:52:45 +0900

    add selenium

--- autoload/leetcode.py
+++ autoload/leetcode.py
@@ -5,6 +5,11 @@ import time
 import os
 from threading import Semaphore, Thread, current_thread
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 try:
     from bs4 import BeautifulSoup
     import requests
